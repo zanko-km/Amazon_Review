0057becd1d7be219876401ce208d8375a0a18d6e
zanko <zankokarimy@gmail.com>
2025-05-06 05:40:46 +0330

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,5 @@
 import subprocess
-subprocess.run(["python", "scripts/db.py"])
-subprocess.run(["python", "scripts/data_preprocessing.py"])
-subprocess.run(["python", "scripts/import_to_db.py"])
-subprocess.run(["python", "scripts/feature_engineering.py"])
+subprocess.run(["python", "Scripts/db.py"])
+subprocess.run(["python", "Scripts/data_preprocessing.py"])
+subprocess.run(["python", "Scripts/import_to_db.py"])
+subprocess.run(["python", "Scripts/feature_engineering.py"])
